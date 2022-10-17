7c4bf0504f6840f4fac14d5c01e5aa1f79613d32
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-17 19:28:39 +0100

    Update print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -1,53 +1,34 @@
 #include "main.h"
 
+/**
+ * print_string - loops through a string and prints
+ * every character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_string(va_list l, flags_t *f)
+{
+	char *s = va_arg(l, char *);
 
+	(void)f;
+
+	if (!s)
+		s = "(null)";
+	return (_puts(s));
+}
 
 /**
-
- * convert - converts number and base into string
-
- * @num: input number
-
- * @base: input base
-
- * @lowercase: flag if hexa values need to be lowercase
-
- * Return: result string
-
+ * print_char - prints a character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-
-char *convert(unsigned long int num, int base, int lowercase)
-
+int print_char(va_list l, flags_t *f)
 {
-
-static char *rep;
-
-static char buffer[50];
-
-char *ptr;
-
-
-
-rep = (lowercase)
-
-? "0123456789abcdef"
-
-: "0123456789ABCDEF";
-
-ptr = &buffer[49];
-
-*ptr = 0;
-
-do {
-
-*--ptr = rep[num % base];
-
-num /= base;
-
-} while (num != 0);
-
-
-
-return (ptr);
-
-} 
+	(void)f;
+	_putchar(va_arg(l, int));
+	return (1);
+}
