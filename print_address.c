2b98d01c9b2aa9d8e251f7dc8e2c430ac42e5271
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-17 19:27:45 +0100

    Update print_address.c

--- print_address.c
+++ print_address.c
@@ -1,53 +1,25 @@
 #include "main.h"
 
-
-
 /**
-
- * convert - converts number and base into string
-
- * @num: input number
-
- * @base: input base
-
- * @lowercase: flag if hexa values need to be lowercase
-
- * Return: result string
-
+ * print_address - prints address of input in hexa format
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-
-char *convert(unsigned long int num, int base, int lowercase)
-
+int print_address(va_list l, flags_t *f)
 {
+	char *str;
+	unsigned long int p = va_arg(l, unsigned long int);
 
-static char *rep;
+	register int count = 0;
 
-static char buffer[50];
+	(void)f;
 
-char *ptr;
-
-
-
-rep = (lowercase)
-
-? "0123456789abcdef"
-
-: "0123456789ABCDEF";
-
-ptr = &buffer[49];
-
-*ptr = 0;
-
-do {
-
-*--ptr = rep[num % base];
-
-num /= base;
-
-} while (num != 0);
-
-
-
-return (ptr);
-
-} 
+	if (!p)
+		return (_puts("(nil)"));
+	str = convert(p, 16, 1);
+	count += _puts("0x");
+	count += _puts(str);
+	return (count);
+}
