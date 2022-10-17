9b2622349e9988dfbd32d4be7b28a74232213541
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-17 19:21:30 +0100

    Update get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -1,53 +1,32 @@
 #include "main.h"
 
-
-
 /**
-
- * convert - converts number and base into string
-
- * @num: input number
-
- * @base: input base
-
- * @lowercase: flag if hexa values need to be lowercase
-
- * Return: result string
-
+ * get_flag - turns on flags if _printf finds
+ * a flag modifier in the format string
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ *
+ * Return: 1 if a flag has been turned on, 0 otherwise
  */
-
-char *convert(unsigned long int num, int base, int lowercase)
-
+int get_flag(char s, flags_t *f)
 {
+	int i = 0;
 
-static char *rep;
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
 
-static char buffer[50];
-
-char *ptr;
-
-
-
-rep = (lowercase)
-
-? "0123456789abcdef"
-
-: "0123456789ABCDEF";
-
-ptr = &buffer[49];
-
-*ptr = 0;
-
-do {
-
-*--ptr = rep[num % base];
-
-num /= base;
-
-} while (num != 0);
-
-
-
-return (ptr);
-
-} 
+	return (i);
+}
