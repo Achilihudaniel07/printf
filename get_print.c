5af9b97709509b0460231c2fd4f83cc863a99c87
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-17 19:22:06 +0100

    Update get_print.c

--- get_print.c
+++ get_print.c
@@ -1,53 +1,39 @@
 #include "main.h"
 
-
-
 /**
-
- * convert - converts number and base into string
-
- * @num: input number
-
- * @base: input base
-
- * @lowercase: flag if hexa values need to be lowercase
-
- * Return: result string
-
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
  */
-
-char *convert(unsigned long int num, int base, int lowercase)
-
+int (*get_print(char s))(va_list, flags_t *)
 {
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
 
-static char *rep;
+	register int i;
 
-static char buffer[50];
-
-char *ptr;
-
-
-
-rep = (lowercase)
-
-? "0123456789abcdef"
-
-: "0123456789ABCDEF";
-
-ptr = &buffer[49];
-
-*ptr = 0;
-
-do {
-
-*--ptr = rep[num % base];
-
-num /= base;
-
-} while (num != 0);
-
-
-
-return (ptr);
-
-} 
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
+}
