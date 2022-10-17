0466b59c9ebafde03f284df70176416a39a21a17
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-17 19:27:03 +0100

    Update print.c

--- print.c
+++ print.c
@@ -1,53 +1,47 @@
 #include "main.h"
 
-
-
 /**
-
- * convert - converts number and base into string
-
- * @num: input number
-
- * @base: input base
-
- * @lowercase: flag if hexa values need to be lowercase
-
- * Return: result string
-
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
-
-char *convert(unsigned long int num, int base, int lowercase)
-
+int _printf(const char *format, ...)
 {
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-static char *rep;
+	register int count = 0;
 
-static char buffer[50];
-
-char *ptr;
-
-
-
-rep = (lowercase)
-
-? "0123456789abcdef"
-
-: "0123456789ABCDEF";
-
-ptr = &buffer[49];
-
-*ptr = 0;
-
-do {
-
-*--ptr = rep[num % base];
-
-num /= base;
-
-} while (num != 0);
-
-
-
-return (ptr);
-
-} 
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+}
