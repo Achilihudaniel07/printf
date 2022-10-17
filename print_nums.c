c03b7f6c4d76ac9f72e3fcff90bc92310e8770fb
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-17 19:32:44 +0100

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -1,53 +1,84 @@
 #include "main.h"
 
+/**
+ * print_int - prints an integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_int(va_list l, flags_t *f)
+{
+	int n = va_arg(l, int);
+	int res = count_digit(n);
 
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+	if (n <= 0)
+		res++;
+	print_number(n);
+	return (res);
+}
 
 /**
-
- * convert - converts number and base into string
-
- * @num: input number
-
- * @base: input base
-
- * @lowercase: flag if hexa values need to be lowercase
-
- * Return: result string
-
+ * print_unsigned - prints an unsigned integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-
-char *convert(unsigned long int num, int base, int lowercase)
-
+int print_unsigned(va_list l, flags_t *f)
 {
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
-static char *rep;
+	(void)f;
+	return (_puts(str));
+}
 
-static char buffer[50];
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
 
-char *ptr;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
 
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
 
+/**
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
 
-rep = (lowercase)
-
-? "0123456789abcdef"
-
-: "0123456789ABCDEF";
-
-ptr = &buffer[49];
-
-*ptr = 0;
-
-do {
-
-*--ptr = rep[num % base];
-
-num /= base;
-
-} while (num != 0);
-
-
-
-return (ptr);
-
-} 
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
+}
