aea50a9ec66a49c9cefd3a564d2e54f4d3644a38
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-18 11:19:14 +0100

    Update numbs.c

--- numbs.c
+++ numbs.c
@@ -1,51 +1,82 @@
 #include "main.h"
 
+/**
+ * convert - converter function, a clone of itoa
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ * @params: paramater struct
+ *
+ * Return: string
+ */
+char *convert(long int num, int base, int flags, params_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+	(void)params;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * print_unsigned - prints unsigned integer numbers
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ */
+int print_unsigned(va_list ap, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+}
+
 
 
 /**
-
- * init_params - clears struct fields and reset buf
-
+ * print_address - prints address
+ * @ap: argument pointer
  * @params: the parameters struct
-
- * @ap: the argument pointer
-
- * Return: void
-
+ *
+ * Return: bytes printed
  */
-
-
-
-void init_params(params_t *params, va_list ap)
-
+int print_address(va_list ap, params_t *params)
 {
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
 
-params->unsign = 0;
+	if (!n)
+		return (_puts("(nil)"));
 
-
-
-params->plus_flag = 0;
-
-params->space_flag = 0;
-
-params->hashtag_flag = 0;
-
-params->zero_flag = 0;
-
-params->minus_flag = 0;
-
-
-
-params->width = 0;
-
-params->precision = UINT_MAX;
-
-
-
-params->h_modifier = 0;
-
-params->l_modifier = 0;
-
-(void)ap;
-
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
 }
