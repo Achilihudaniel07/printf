45273c47bc85b4fe40b858a8bd115677086a21f4
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-18 11:12:07 +0100

    Update 2-number.c

--- 2-number.c
+++ 2-number.c
@@ -1,51 +1,79 @@
 #include "main.h"
 
+/**
+ * print_from_to - prints a range of char addresses
+ * @start: starting address
+ * @stop: stopping address
+ * @except: except address
+ *
+ * Return: number bytes printed
+ */
+int print_from_to(char *start, char *stop, char *except)
+{
+	int sum = 0;
 
+	while (start <= stop)
+	{
+		if (start != except)
+			sum += _putchar(*start);
+		start++;
+	}
+	return (sum);
+}
 
 /**
-
- * init_params - clears struct fields and reset buf
-
+ * print_rev - prints string in reverse
+ * @ap: string
  * @params: the parameters struct
-
- * @ap: the argument pointer
-
- * Return: void
-
+ *
+ * Return: number bytes printed
  */
-
-
-
-void init_params(params_t *params, va_list ap)
-
+int print_rev(va_list ap, params_t *params)
 {
+	int len, sum = 0;
+	char *str = va_arg(ap, char *);
+	(void)params;
 
-params->unsign = 0;
+	if (str)
+	{
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			sum += _putchar(*str);
+	}
+	return (sum);
+}
 
+/**
+ * print_rot13 - prints string in rot13
+ * @ap: string
+ * @params: the parameters struct
+ *
+ * Return: number bytes printed
+ */
+int print_rot13(va_list ap, params_t *params)
+{
+	int i, index;
+	int count = 0;
+	char arr[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(ap, char *);
+	(void)params;
 
-
-params->plus_flag = 0;
-
-params->space_flag = 0;
-
-params->hashtag_flag = 0;
-
-params->zero_flag = 0;
-
-params->minus_flag = 0;
-
-
-
-params->width = 0;
-
-params->precision = UINT_MAX;
-
-
-
-params->h_modifier = 0;
-
-params->l_modifier = 0;
-
-(void)ap;
-
+	i = 0;
+	index = 0;
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z')
+		    || (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] - 65;
+			count += _putchar(arr[index]);
+		}
+		else
+			count += _putchar(a[i]);
+		i++;
+	}
+	return (count);
 }
