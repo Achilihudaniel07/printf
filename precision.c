370dbe0609a1aca5193bfcc268efc961dd81f1cd
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-18 11:19:39 +0100

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,51 +1,30 @@
 #include "main.h"
 
-
-
 /**
-
- * init_params - clears struct fields and reset buf
-
+ * get_precision - gets the precision from the format string
+ * @s: the format string
  * @params: the parameters struct
-
  * @ap: the argument pointer
+ * Return: new pointer
+*/
 
- * Return: void
-
- */
-
-
-
-void init_params(params_t *params, va_list ap)
-
+char *get_precision(char *s, params_t *params, va_list ap)
 {
+	int d = 0;
 
-params->unsign = 0;
-
-
-
-params->plus_flag = 0;
-
-params->space_flag = 0;
-
-params->hashtag_flag = 0;
-
-params->zero_flag = 0;
-
-params->minus_flag = 0;
-
-
-
-params->width = 0;
-
-params->precision = UINT_MAX;
-
-
-
-params->h_modifier = 0;
-
-params->l_modifier = 0;
-
-(void)ap;
-
+	if (*s != '.')
+		return (s);
+	s++;
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->precision = d;
+	return (s);
 }
