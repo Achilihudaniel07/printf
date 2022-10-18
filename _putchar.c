481ce79c5575d72082c9e43f09eddd3fa6f17563
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-18 11:15:35 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,51 +1,38 @@
 #include "main.h"
 
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return:( str-a)
+ */
+int _puts(char *str)
+{
+	char *a = str;/*declaration of variables*/
 
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
 
 /**
-
- * init_params - clears struct fields and reset buf
-
- * @params: the parameters struct
-
- * @ap: the argument pointer
-
- * Return: void
-
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
-
-
-
-void init_params(params_t *params, va_list ap)
-
+int _putchar(int c)
 {
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
 
-params->unsign = 0;
-
-
-
-params->plus_flag = 0;
-
-params->space_flag = 0;
-
-params->hashtag_flag = 0;
-
-params->zero_flag = 0;
-
-params->minus_flag = 0;
-
-
-
-params->width = 0;
-
-params->precision = UINT_MAX;
-
-
-
-params->h_modifier = 0;
-
-params->l_modifier = 0;
-
-(void)ap;
-
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
